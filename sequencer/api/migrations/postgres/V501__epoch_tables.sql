318f1801db821a1129f6614d896541b50f343e42
Brendon Fish <bfish713@gmail.com>
2025-03-19 13:19:32 -0400

    remove undecided state 2 from migrations

--- sequencer/api/migrations/postgres/V501__epoch_tables.sql
+++ sequencer/api/migrations/postgres/V501__epoch_tables.sql
@@ -18,16 +18,6 @@ CREATE TABLE vid_share2 (
 );
 
 
-CREATE TABLE undecided_state2 (
-    -- The ID is always set to 0. Setting it explicitly allows us to enforce with every insert or
-    -- update that there is only a single entry in this table: the latest known state.
-    id INT PRIMARY KEY,
-
-    leaves BYTEA NOT NULL,
-    state  BYTEA NOT NULL
-);
-
-
 CREATE TABLE quorum_proposals2 (
     view BIGINT PRIMARY KEY,
     leaf_hash VARCHAR,
