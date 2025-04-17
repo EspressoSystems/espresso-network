ef113abc27e122ddbb4616c7b6da53b0874b3636
tbro <tbro@users.noreply.github.com>
2025-04-17 13:44:57 -0300

    merge conflict fixes
    Mostly from:
    
      * add tests for rewards (#3044)
      * duplicate rand-chacha entry in Cargo.toml

--- staking-cli/src/demo.rs
+++ staking-cli/src/demo.rs
@@ -1,10 +1,16 @@
 use alloy::{
-    network::{EthereumWallet, TransactionBuilder as _},
+    network::{Ethereum, EthereumWallet, TransactionBuilder as _},
     primitives::{
         utils::{format_ether, parse_ether},
         Address, U256,
     },
-    providers::{Provider, ProviderBuilder, WalletProvider},
+    providers::{
+        fillers::{
+            BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller, TxFiller,
+            WalletFiller,
+        },
+        Identity, Provider, ProviderBuilder, RootProvider, WalletProvider,
+    },
     rpc::types::TransactionRequest,
     signers::{
         k256::ecdsa::SigningKey,
@@ -21,7 +27,7 @@ use hotshot_types::{
     signature_key::{BLSKeyPair, BLSPubKey},
     traits::{signature_key::SignatureKey, stake_table::StakeTableScheme},
 };
-use rand::SeedableRng;
+use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha20Rng;
 use sequencer_utils::deployer::{self, Contract, Contracts};
 use url::Url;
@@ -36,6 +42,23 @@ use crate::{
 pub type XStakeTable = StakeTable<BLSPubKey, StateVerKey, CircuitField>;
 
 pub const STAKE_TABLE_CAPACITY_FOR_TEST: u64 = 3;
+type Prov = FillProvider<
+    JoinFill<
+        JoinFill<
+            Identity,
+            JoinFill<GasFiller, JoinFill<BlobGasFiller, JoinFill<NonceFiller, ChainIdFiller>>>,
+        >,
+        WalletFiller<EthereumWallet>,
+    >,
+    RootProvider,
+>;
+
+fn make_provider(rpc_url: &Url, signer: LocalSigner<SigningKey>) -> Prov {
+    let wallet = EthereumWallet::from(signer);
+    ProviderBuilder::new()
+        .wallet(wallet)
+        .on_http(rpc_url.clone())
+}
 
 pub async fn stake_in_contract_for_test(
     rpc_url: Url,
@@ -43,19 +66,13 @@ pub async fn stake_in_contract_for_test(
     stake_table_address: Address,
     token_address: Address,
     validator_keys: Vec<(PrivateKeySigner, BLSKeyPair, StateKeyPair)>,
+    multiple_delegators: bool,
 ) -> Result<()> {
     tracing::info!("staking to stake table contract for demo");
 
-    let mk_provider = |signer| {
-        let wallet = EthereumWallet::from(signer);
-        ProviderBuilder::new()
-            .wallet(wallet)
-            .on_http(rpc_url.clone())
-    };
-
     tracing::info!("stake table address: {}", stake_table_address);
 
-    let token_signer = mk_provider(grant_recipient.clone());
+    let token_signer = make_provider(&rpc_url, grant_recipient.clone());
 
     tracing::info!("ESP token address: {token_address}");
     let token = EspToken::new(token_address, token_signer.clone());
@@ -69,19 +86,23 @@ pub async fn stake_in_contract_for_test(
         panic!("grant recipient has no ESP tokens, funding won't work");
     }
 
-    let fund_amount_eth = "1000";
-    let fund_amount = parse_ether(fund_amount_eth)?;
+    let fund_amount_esp = parse_ether("1000")?;
+    let fund_amount_eth = parse_ether("10")?;
+
+    // Set up deterministic rng
+    let seed = [42u8; 32];
+    let mut rng = ChaCha20Rng::from_seed(seed);
 
     for (val_index, (signer, bls_key_pair, state_key_pair)) in
         validator_keys.into_iter().enumerate()
     {
-        let validator_provider = mk_provider(signer);
+        let validator_provider = make_provider(&rpc_url, signer);
         let validator_address = validator_provider.default_signer_address();
 
         tracing::info!("fund val {val_index} address: {validator_address}, {fund_amount_eth} ETH");
         let tx = TransactionRequest::default()
             .with_to(validator_address)
-            .with_value(fund_amount);
+            .with_value(fund_amount_eth);
         let receipt = token_signer
             .send_transaction(tx)
             .await?
@@ -100,19 +121,19 @@ pub async fn stake_in_contract_for_test(
 
         tracing::info!("validator {val_index} address: {validator_address}, balance: {bal}");
 
-        tracing::info!("transfer {fund_amount_eth} ESP to {validator_address}",);
+        tracing::info!("transfer {fund_amount_esp} ESP to {validator_address}",);
         let receipt = token
-            .transfer(validator_address, fund_amount)
+            .transfer(validator_address, fund_amount_esp)
             .send()
             .await?
             .get_receipt()
             .await?;
         assert!(receipt.status());
 
-        tracing::info!("approve {fund_amount_eth} ESP for {stake_table_address}",);
+        tracing::info!("approve {fund_amount_esp} ESP for {stake_table_address}",);
         let validator_token = EspToken::new(token_address, validator_provider.clone());
         let receipt = validator_token
-            .approve(stake_table_address, fund_amount)
+            .approve(stake_table_address, fund_amount_esp)
             .send()
             .await?
             .get_receipt()
@@ -142,8 +163,101 @@ pub async fn stake_in_contract_for_test(
         )
         .await?;
         assert!(receipt.status());
+
+        if multiple_delegators {
+            tracing::info!("adding multiple delegators for validator  {val_index} ");
+
+            let num_delegators = rng.gen_range(2..=5);
+
+            add_multiple_delegators(
+                &rpc_url,
+                validator_address,
+                &token_signer,
+                &token,
+                stake_table_address,
+                token_address,
+                &mut rng,
+                num_delegators,
+            )
+            .await?;
+        }
     }
     tracing::info!("completed staking for demo");
+    Ok(())
+}
+
+#[allow(clippy::too_many_arguments)]
+async fn add_multiple_delegators<F: TxFiller<Ethereum>, P: Provider<Ethereum>>(
+    rpc_url: &Url,
+    validator_address: Address,
+    token_signer: &FillProvider<F, P, Ethereum>,
+    token: &EspToken::EspTokenInstance<(), FillProvider<F, P, Ethereum>>,
+    stake_table_address: Address,
+    token_address: Address,
+    rng: &mut ChaCha20Rng,
+    num_delegators: u64,
+) -> Result<()> {
+    let fund_amount_esp = parse_ether("1000")?;
+    let fund_amount_eth = parse_ether("10")?;
+
+    for delegator_index in 0..num_delegators {
+        let delegator_wallet: LocalSigner<SigningKey> = SigningKey::random(rng).into();
+        let delegator_address = delegator_wallet.address();
+        tracing::info!("delegator {delegator_index}: address {delegator_address}");
+
+        let tx = TransactionRequest::default()
+            .with_to(delegator_address)
+            .with_value(fund_amount_eth);
+        let receipt = token_signer
+            .send_transaction(tx)
+            .await?
+            .get_receipt()
+            .await?;
+        assert!(receipt.status());
+
+        tracing::info!("delegator {delegator_index}: funded with {fund_amount_eth} ETH");
+
+        let random_amount: u64 = rng.gen_range(100..=500);
+        let delegate_amount = parse_ether(&random_amount.to_string())?;
+        let delegate_amount_esp = format_ether(delegate_amount);
+
+        let receipt = token
+            .transfer(delegator_address, fund_amount_esp)
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
+        assert!(receipt.status());
+
+        tracing::info!("delegator {delegator_index}: received {fund_amount_esp} ESP");
+
+        let delegator_provider = make_provider(rpc_url, delegator_wallet.clone());
+
+        let validator_token = EspToken::new(token_address, delegator_provider.clone());
+        let receipt = validator_token
+            .approve(stake_table_address, delegate_amount)
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
+        assert!(receipt.status());
+
+        tracing::info!(
+            "delegator {delegator_index}: approved {delegate_amount_esp} ESP to stake table"
+        );
+
+        let receipt = delegate(
+            &delegator_provider,
+            stake_table_address,
+            validator_address,
+            delegate_amount,
+        )
+        .await?;
+        assert!(receipt.status());
+
+        tracing::info!("delegator {delegator_index}: delegation complete");
+    }
+
     Ok(())
 }
 
@@ -198,6 +312,7 @@ pub async fn stake_for_demo(config: &Config, num_validators: u16) -> Result<()> 
         config.stake_table_address,
         config.token_address,
         validator_keys,
+        false,
     )
     .await?;
 
@@ -274,6 +389,7 @@ pub async fn pos_deploy_routine(
             .address(Contract::EspTokenProxy)
             .expect("ESP token deployed"),
         staking_priv_keys,
+        false,
     )
     .await?;
 
@@ -307,7 +423,7 @@ mod test {
     use hotshot_types::{traits::signature_key::StakeTableEntryType, PeerConfig};
 
     use super::*;
-    use crate::{deploy::TestSystem, l1::decode_log};
+    use crate::deploy::TestSystem;
 
     fn mock_stake(n: u16) -> StakeTable {
         [..n]
