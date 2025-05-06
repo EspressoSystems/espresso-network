dd34561899a0028ebfb9072617bca74220f8458b
sveitser <sveitser@gmail.com>
2025-05-06 18:50:44 +0200

    CI: further split tests_2

--- crates/hotshot/testing/tests/test_epoch_success_types_randomized_leader.rs
+++ crates/hotshot/testing/tests/test_epoch_success_types_randomized_leader.rs
@@ -7,10 +7,8 @@
 use std::time::Duration;
 
 use hotshot_example_types::node_types::{
-        EpochsTestVersions, Libp2pImpl, MemoryImpl,
-        PushCdnImpl, RandomOverlapQuorumFilterConfig, StableQuorumFilterConfig, TestTypes, TestTypesEpochCatchupTypes,
-        TestTypesRandomizedCommitteeMembers, TestTypesRandomizedLeader,
-    };
+    EpochsTestVersions, Libp2pImpl, MemoryImpl, PushCdnImpl, TestTypesRandomizedLeader,
+};
 use hotshot_macros::cross_tests;
 use hotshot_testing::{
     block_builder::SimpleBuilderImplementation,
@@ -22,15 +20,7 @@ cross_tests!(
     TestName: test_epoch_success,
     Impls: [MemoryImpl, Libp2pImpl, PushCdnImpl],
     Types: [
-        TestTypes,
-        TestTypesEpochCatchupTypes,
         TestTypesRandomizedLeader,
-        TestTypesRandomizedCommitteeMembers<StableQuorumFilterConfig<123, 2>>,                 // Overlap =  F
-        TestTypesRandomizedCommitteeMembers<StableQuorumFilterConfig<123, 3>>,                 // Overlap =  F+1
-        TestTypesRandomizedCommitteeMembers<StableQuorumFilterConfig<123, 4>>,                 // Overlap = 2F
-        TestTypesRandomizedCommitteeMembers<StableQuorumFilterConfig<123, 5>>,                 // Overlap = 2F+1
-        TestTypesRandomizedCommitteeMembers<StableQuorumFilterConfig<123, 6>>,                 // Overlap = 3F
-        TestTypesRandomizedCommitteeMembers<RandomOverlapQuorumFilterConfig<123, 4, 7, 0, 2>>, // Overlap = Dynamic
     ],
     Versions: [EpochsTestVersions],
     Ignore: false,
