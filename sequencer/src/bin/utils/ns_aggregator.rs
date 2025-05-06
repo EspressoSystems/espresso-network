a419fc21c5eb4468b7d0d04028f234d6d3d84b2f
Jeb Bearer <jeb@espressosys.com>
2025-05-06 15:07:47 -0700

    Clear up from,to,start,end parameters

--- sequencer/src/bin/utils/ns_aggregator.rs
+++ sequencer/src/bin/utils/ns_aggregator.rs
@@ -1,26 +1,38 @@
 use std::cmp::min;
 
-use anyhow::ensure;
+use anyhow::{ensure, Context};
 use clap::Parser;
 use espresso_types::{Header, NamespaceId, NamespaceProofQueryData};
 use futures::{future::try_join_all, stream::StreamExt};
 use sequencer::SequencerApiVersion;
 use surf_disco::Url;
 
+/// Count transactions and bytes confirmed in a given namespace.
 #[derive(Debug, Parser)]
 pub struct Options {
+    /// Block to start counting from.
     #[clap(short, long, default_value = "0")]
     from_block: usize,
 
+    /// Last block to count (inclusive).
+    ///
+    /// If not specified, will count until the end of the chain at the moment the program is
+    /// invoked.
     #[clap(short, long)]
     to_block: Option<usize>,
 
+    /// Namespace to aggregate.
     #[clap(short, long)]
     namespace: u64,
 
+    /// Number of parallel tasks to run.
+    ///
+    /// Since the process is mostly I/O bound, increasing this can dramatically speed up
+    /// aggregation.
     #[clap(short, long, default_value = "1")]
     jobs: usize,
 
+    /// Espresso query service URL.
     url: Url,
 }
 
@@ -30,18 +42,20 @@ pub async fn run(opt: Options) -> anyhow::Result<()> {
         opt.url.clone(),
     );
 
-    let from = opt.from_block;
-    let to = match opt.to_block {
-        Some(to) => to,
+    // Convert optional closed [from, to] interval to a semi-open [start, end) interval, which makes
+    // arithmetic simpler later on.
+    let start = opt.from_block;
+    let end = match opt.to_block {
+        Some(to) => to + 1,
         None => client.get("status/block-height").send().await?,
     };
-    ensure!(to >= from, "to-block < from-block");
+    ensure!(end > start, "to-block < from-block");
 
     let tasks = (0..opt.jobs).map(|i| {
-        let chunk_size = (to - from) / opt.jobs;
-        let start = from + i * chunk_size;
-        let end = min(start + chunk_size, to);
-        process_chunk(opt.url.clone(), ns, start, end)
+        let chunk_size = (end - start) / opt.jobs;
+        let chunk_start = start + i * chunk_size;
+        let chunk_end = min(chunk_start + chunk_size, end);
+        process_chunk(opt.url.clone(), ns, chunk_start, chunk_end)
     });
     let chunks = try_join_all(tasks).await?;
 
@@ -52,7 +66,7 @@ pub async fn run(opt: Options) -> anyhow::Result<()> {
         bytes += chunk_bytes;
     }
 
-    println!("Scanned range [{from}, {to}) for namespace {ns}");
+    println!("Scanned range [{start}, {end}) for namespace {ns}");
     println!("{num_txs} transactions");
     println!("{bytes} bytes");
 
@@ -62,15 +76,15 @@ pub async fn run(opt: Options) -> anyhow::Result<()> {
 async fn process_chunk(
     url: Url,
     ns: NamespaceId,
-    from: usize,
-    to: usize,
+    start: usize,
+    end: usize,
 ) -> anyhow::Result<(usize, usize)> {
     let client = surf_disco::Client::<hotshot_query_service::Error, SequencerApiVersion>::new(url);
     let mut headers = client
-        .socket(&format!("availability/stream/headers/{from}"))
+        .socket(&format!("availability/stream/headers/{start}"))
         .subscribe::<Header>()
         .await?
-        .take(to - from);
+        .take(end - start);
 
     let mut num_txs = 0;
     let mut bytes = 0;
@@ -87,7 +101,8 @@ async fn process_chunk(
         let payload: NamespaceProofQueryData = client
             .get(&format!("availability/block/{height}/namespace/{ns}",))
             .send()
-            .await?;
+            .await
+            .context(format!("requesting namespace for block {height}"))?;
         num_txs += payload.transactions.len();
         bytes += payload
             .transactions
