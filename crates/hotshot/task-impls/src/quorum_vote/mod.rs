e203b283cff3ccc0864ae00d33b08fa7c166ac63
Lukasz Rzasik <lukasz.rzasik@gmail.com>
2025-05-07 16:30:31 +0200

    Decide events include only old epoch vid shares

--- crates/hotshot/task-impls/src/quorum_vote/mod.rs
+++ crates/hotshot/task-impls/src/quorum_vote/mod.rs
@@ -225,7 +225,7 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES> + 'static, V: Versions> Handl
             return;
         };
 
-        let mut maybe_next_epoch_vid_share = None;
+        let mut maybe_current_epoch_vid_share = None;
         // If this is an epoch transition block, we might need two VID shares.
         if self.upgrade_lock.epochs_enabled(leaf.view_number()).await
             && is_epoch_transition(leaf.block_header().block_number(), self.epoch_height)
@@ -259,9 +259,9 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES> + 'static, V: Versions> Handl
                 && next_epoch_membership.has_stake(&self.public_key).await
             {
                 let other_target_epoch = if vid_share.data.target_epoch() == current_epoch {
+                    maybe_current_epoch_vid_share = Some(vid_share.clone());
                     next_epoch
                 } else {
-                    maybe_next_epoch_vid_share = Some(vid_share.clone());
                     current_epoch
                 };
                 match wait_for_second_vid_share(
@@ -276,8 +276,8 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES> + 'static, V: Versions> Handl
                 .await
                 {
                     Ok(other_vid_share) => {
-                        if maybe_next_epoch_vid_share.is_none() {
-                            maybe_next_epoch_vid_share = Some(other_vid_share);
+                        if maybe_current_epoch_vid_share.is_none() {
+                            maybe_current_epoch_vid_share = Some(other_vid_share);
                         }
                     },
                     Err(e) => {
@@ -303,7 +303,7 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES> + 'static, V: Versions> Handl
             self.view_number,
             Arc::clone(&self.instance_state),
             &leaf,
-            maybe_next_epoch_vid_share.as_ref().unwrap_or(&vid_share),
+            maybe_current_epoch_vid_share.as_ref().unwrap_or(&vid_share),
             parent_view_number,
             self.epoch_height,
         )
@@ -364,7 +364,7 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES> + 'static, V: Versions> Handl
                 self.view_number,
                 self.storage.clone(),
                 leaf,
-                maybe_next_epoch_vid_share.unwrap_or(vid_share),
+                maybe_current_epoch_vid_share.unwrap_or(vid_share),
                 is_vote_leaf_extended,
                 is_vote_epoch_root,
                 self.epoch_height,
