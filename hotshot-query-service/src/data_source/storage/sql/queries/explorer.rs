dd75eef707072adf99ba97615c02e48f132473d2
Jeb Bearer <jeb@espressosys.com>
2025-05-27 20:11:18 -0700

    Fix explorer queries

--- hotshot-query-service/src/data_source/storage/sql/queries/explorer.rs
+++ hotshot-query-service/src/data_source/storage/sql/queries/explorer.rs
@@ -330,14 +330,14 @@ where
         // returned results based on.
         let transaction_target_query = match target {
             TransactionIdentifier::Latest => query(
-                "SELECT block_height AS height, namespace, position FROM transactions ORDER BY height DESC, namespace DESC, position DESC LIMIT 1",
+                "SELECT block_height AS height, namespace, position FROM transactions ORDER BY block_height DESC, namespace DESC, position DESC LIMIT 1",
             ),
             TransactionIdentifier::HeightAndOffset(height, _) => query(
-                "SELECT block_height AS height, namespace, position FROM transactions WHERE height = $1 ORDER BY namespace DESC, position DESC LIMIT 1",
+                "SELECT block_height AS height, namespace, position FROM transactions WHERE block_height = $1 ORDER BY namespace DESC, position DESC LIMIT 1",
             )
             .bind(*height as i64),
             TransactionIdentifier::Hash(hash) => query(
-                "SELECT block_height AS height, namespace, position FROM transactions WHERE hash = $1 ORDER BY height DESC, namespace DESC, position DESC LIMIT 1",
+                "SELECT block_height AS height, namespace, position FROM transactions WHERE hash = $1 ORDER BY block_height DESC, namespace DESC, position DESC LIMIT 1",
             )
             .bind(hash.to_string()),
         };
