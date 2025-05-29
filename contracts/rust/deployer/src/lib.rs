a045c54761bb62270938eaea5f1179329be52f16
Alysia Huggins <hi@alysia.tech>
2025-05-29 14:55:22 -0400

    rust lint

--- contracts/rust/deployer/src/lib.rs
+++ contracts/rust/deployer/src/lib.rs
@@ -619,8 +619,7 @@ pub async fn upgrade_light_client_v2_multisig_owner(
 
                 // get initialized version number from proxy
                 let proxy_addr = contracts.address(Contract::LightClientProxy).unwrap();
-                let initialized =
-                    get_proxy_initialized_version(&provider, proxy_addr.clone()).await?;
+                let initialized = get_proxy_initialized_version(&provider, proxy_addr).await?;
 
                 // get contract version from proxy
                 let lcv2_proxy = LightClientV2::new(proxy_addr, &provider);
