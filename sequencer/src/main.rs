637cde5d31d4bb714f44dbef68681750fcef003e
Rob <rob@espressosys.com>
2025-06-04 13:33:57 -0400

    nice

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -1,9 +1,35 @@
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
+
+use tokio::spawn;
+
 #[global_allocator]
 static ALLOC: dhat::Alloc = dhat::Alloc;
 
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
-    let _profiler = dhat::Profiler::new_heap();
+    let profiler = dhat::Profiler::new_heap();
+
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    spawn(async move {
+        while running.load(Ordering::SeqCst) {
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
+        drop(profiler);
+        std::process::exit(0);
+    });
 
     sequencer::main().await
 }
