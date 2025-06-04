acf691a60cda7d9fd8bc239c881cb6dbd47d5d40
Rob <rob@espressosys.com>
2025-06-04 13:04:57 -0400

    dhat

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -1,4 +1,9 @@
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
+    let _profiler = dhat::Profiler::new_heap();
+
     sequencer::main().await
 }
