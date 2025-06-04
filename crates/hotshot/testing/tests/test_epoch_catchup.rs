04004301aa34ad10579811f439bf43d02301928f
Lukasz Rzasik <lukasz.rzasik@gmail.com>
2025-06-04 17:22:08 +0200

    Add test_two_stake_tables_catchup_epochs

--- crates/hotshot/testing/tests/test_epoch_catchup.rs
+++ crates/hotshot/testing/tests/test_epoch_catchup.rs
@@ -7,8 +7,9 @@
 use std::time::Duration;
 
 use hotshot_example_types::node_types::{
-    EpochsTestVersions, PushCdnImpl, TestTypes, TestTypesEpochCatchupTypes,
+    EpochsTestVersions, PushCdnImpl, TestTwoStakeTablesTypes, TestTypes, TestTypesEpochCatchupTypes,
 };
+use hotshot_macros::cross_tests;
 use hotshot_testing::{
     block_builder::SimpleBuilderImplementation,
     completion_task::{CompletionTaskDescription, TimeBasedCompletionTaskDescription},
@@ -17,52 +18,100 @@ use hotshot_testing::{
     test_builder::{TestDescription, TimingData},
 };
 
-#[cfg(test)]
-#[tokio::test(flavor = "multi_thread")]
-async fn test_catchup_epochs() {
-    hotshot::helpers::initialize_logging();
-
-    let timing_data = TimingData {
-        next_view_timeout: 5000,
-        ..Default::default()
-    };
-    let mut metadata: TestDescription<
+cross_tests!(
+    TestName: test_catchup_epochs,
+    Impls: [PushCdnImpl],
+    Types: [
         TestTypesEpochCatchupTypes<EpochsTestVersions, TestTypes>,
-        PushCdnImpl,
-        EpochsTestVersions,
-    > = TestDescription::default().set_num_nodes(20, 7);
-    let catchup_node = vec![ChangeNode {
-        idx: 19,
-        updown: NodeAction::Up,
-    }];
+    ],
+    Versions: [EpochsTestVersions],
+    Ignore: false,
+    Metadata: {
+        let timing_data = TimingData {
+            next_view_timeout: 5000,
+            ..Default::default()
+        };
 
-    metadata.timing_data = timing_data;
+        let mut metadata = TestDescription::default().set_num_nodes(20, 7);
 
-    metadata.view_sync_properties =
-        hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 20);
+        let catchup_node = vec![ChangeNode {
+            idx: 19,
+            updown: NodeAction::Up,
+        }];
 
-    metadata.spinning_properties = SpinningTaskDescription {
-        node_changes: vec![(35, catchup_node)],
-    };
+        metadata.timing_data = timing_data;
 
-    metadata.completion_task_description =
-        CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
-            TimeBasedCompletionTaskDescription {
-                duration: Duration::from_secs(120),
-            },
-        );
-    metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
-        num_successful_views: 50,
-        possible_view_failures: vec![18, 19],
-        decide_timeout: Duration::from_secs(15),
-        ..Default::default()
-    };
+        metadata.view_sync_properties =
+            hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 20);
 
-    metadata.skip_late = true;
+        metadata.spinning_properties = SpinningTaskDescription {
+            node_changes: vec![(35, catchup_node)],
+        };
 
-    metadata
-        .gen_launcher()
-        .launch()
-        .run_test::<SimpleBuilderImplementation>()
-        .await;
-}
+        metadata.completion_task_description =
+            CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
+                TimeBasedCompletionTaskDescription {
+                    duration: Duration::from_secs(120),
+                },
+            );
+        metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
+            num_successful_views: 50,
+            possible_view_failures: vec![18, 19],
+            decide_timeout: Duration::from_secs(15),
+            ..Default::default()
+        };
+
+        metadata.skip_late = true;
+
+        metadata
+    },
+);
+
+cross_tests!(
+    TestName: test_two_stake_tables_catchup_epochs,
+    Impls: [PushCdnImpl],
+    Types: [
+        TestTypesEpochCatchupTypes<EpochsTestVersions, TestTwoStakeTablesTypes>,
+    ],
+    Versions: [EpochsTestVersions],
+    Ignore: false,
+    Metadata: {
+        let timing_data = TimingData {
+            next_view_timeout: 5000,
+            ..Default::default()
+        };
+
+        let mut metadata = TestDescription::default().set_num_nodes(20, 7);
+
+        let catchup_node = vec![ChangeNode {
+            idx: 18,
+            updown: NodeAction::Up,
+        }];
+
+        metadata.timing_data = timing_data;
+
+        metadata.view_sync_properties =
+            hotshot_testing::view_sync_task::ViewSyncTaskDescription::Threshold(0, 20);
+
+        metadata.spinning_properties = SpinningTaskDescription {
+            node_changes: vec![(35, catchup_node)],
+        };
+
+        metadata.completion_task_description =
+            CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
+                TimeBasedCompletionTaskDescription {
+                    duration: Duration::from_secs(120),
+                },
+            );
+        metadata.overall_safety_properties = OverallSafetyPropertiesDescription {
+            num_successful_views: 50,
+            possible_view_failures: vec![18, 19],
+            decide_timeout: Duration::from_secs(15),
+            ..Default::default()
+        };
+
+        metadata.skip_late = true;
+
+        metadata
+    },
+);
