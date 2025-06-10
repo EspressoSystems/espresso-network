e987ec820c619382865e4fc344220f42bd225ed8
pls148 <184445976+pls148@users.noreply.github.com>
2025-06-09 17:22:48 -0700

    Non-uniform stake testing ability

---
+++ crates/hotshot/testing/src/node_stake.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+use alloy::primitives::U256;
+
+#[derive(Clone)]
+pub struct TestNodeStakes {
+    stakes: HashMap<u64, U256>,
+    default_stake: U256,
+}
+
+impl TestNodeStakes {
+    pub fn new(stakes: HashMap<u64, U256>, default_stake: U256) -> Self {
+        Self {
+            stakes,
+            default_stake,
+        }
+    }
+
+    pub fn get(&self, node_id: u64) -> U256 {
+        self.stakes
+            .get(&node_id)
+            .cloned()
+            .unwrap_or_else(|| self.default_stake)
+    }
+}
+
+impl Default for TestNodeStakes {
+    fn default() -> Self {
+        Self {
+            stakes: HashMap::new(),
+            default_stake: U256::from(1),
+        }
+    }
+}
