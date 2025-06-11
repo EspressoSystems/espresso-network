eb45593d0dd1e5c3d4bc4dc91050e8ec91204faa
imabdulbasit <baronbasit@gmail.com>
2025-06-11 14:56:13 +0500

    add comments

--- hotshot-query-service/migrations/sqlite/V800__aggregate_table_ns.sql
+++ hotshot-query-service/migrations/sqlite/V800__aggregate_table_ns.sql
@@ -1,3 +1,7 @@
+-- this table is dropped so that we can start building aggregator stats from block height 0
+-- for each namespace
+-- otherwise aggregator would have namespace stats for future blocks only
+
 DROP TABLE IF EXISTS aggregate;
 
 CREATE TABLE aggregate (
