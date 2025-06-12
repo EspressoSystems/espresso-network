5d21c3c8175ff3af80929306e65399353509275e
sveitser <sveitser@gmail.com>
2025-06-12 10:29:21 +0200

    improve error message again

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -4,12 +4,13 @@ compile_error!(
 The `embedded-db` feature is enabled, but the sequencer binary is not compatible
 with the `embedded-db` feature. Aborting build.
 
+If the intention is to build the sequencer-sqlite binary run `cargo build -p
+sequencer-sqlite` instead.
+
 To build the (postgres) sequencer binary make sure the embedded-db feature is
 disabled and the sequencer-sqlite crate is **not** part of the build. Including
-it will enable the `embedded-db` feature globally.
-
-To build the sequencer-sqlite binary run `cargo build -p sequencer-sqlite`
-instead.
+the sequencer-sqlite crate in the build will enable the `embedded-db` feature
+globally.
 
 By default the sequencer-sqlite workspace crate is excluded from cargo
 invocations because it's not a default workspace member. Avoid using the
