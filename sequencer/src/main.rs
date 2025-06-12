89d0ee78e4497cdbeb34e30c48a69d6cd6698fef
sveitser <sveitser@gmail.com>
2025-06-12 09:36:26 +0200

    fail build on incompatible feature

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "embedded-db")]
+compile_error!("The sequencer binary is not compatible with the 'embedded-db' feature, compile the sequencer-sqlite crate instead");
+
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
     sequencer::main().await
