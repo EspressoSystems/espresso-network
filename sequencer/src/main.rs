fca64e172acb4211be6aa0deed8a0e3c20478aba
sveitser <sveitser@gmail.com>
2025-06-12 10:16:54 +0200

    more descriptive compilation error

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -1,5 +1,23 @@
 #[cfg(all(feature = "embedded-db", not(clippy)))]
-compile_error!("The sequencer binary is not compatible with the 'embedded-db' feature, compile the sequencer-sqlite crate instead");
+compile_error!(
+    r#"
+The `embedded-db` feature is enabled, but the sequencer binary is not compatible
+with the `embedded-db` feature. Aborting build.
+
+To build the (postgres) sequencer binary make sure the embedded-db feature is
+disabled and the sequencer-sqlite crate is **not** part of the build. Including
+it will enable the `embedded-db` feature globally.
+
+To build the sequencer-sqlite binary run `cargo build -p sequencer-sqlite`
+instead.
+
+By default the sequencer-sqlite workspace crate is excluded from cargo
+invocations because it's not a default workspace member. Avoid using the
+`--workspace` cargo flag unless enabling the `embedded-db` feature (via inclusion
+of the sequencer-sqlite crate in the build) is intended.
+
+"#
+);
 
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
