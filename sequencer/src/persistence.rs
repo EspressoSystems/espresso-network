c0ba91d7cece439cd1e54d201b74df1adda99beb
alysiahuggins <hi@alysia.tech>
2025-08-13 16:08:40 -0400

    adjust the exitescrowperiod in tests so that it is compliant

--- sequencer/src/persistence.rs
+++ sequencer/src/persistence.rs
@@ -1460,6 +1460,7 @@ mod tests {
             .genesis_st_state(genesis_stake)
             .blocks_per_epoch(blocks_per_epoch)
             .epoch_start_block(1)
+            .exit_escrow_period(U256::from(blocks_per_epoch * 15 + 100))
             .multisig_pauser(network_config.signer().address())
             .token_name("Espresso".to_string())
             .token_symbol("ESP".to_string())
