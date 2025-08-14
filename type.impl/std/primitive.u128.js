91dfadc33050314dc6de8bc4adb09c0b2d36f785
shenkeyao <shenkeyao@users.noreply.github.com>
2025-08-14 16:27:07 +0000

    deploy: d2f862078d9906466d51142981852b6ff971e3f6

--- type.impl/std/primitive.u128.js
+++ type.impl/std/primitive.u128.js
@@ -1,9 +1,9 @@
 (function() {
-    var type_impls = Object.fromEntries([["hotshot_builder_core",[]]]);
+    var type_impls = Object.fromEntries([["hotshot_builder_legacy",[]]]);
     if (window.register_type_impls) {
         window.register_type_impls(type_impls);
     } else {
         window.pending_type_impls = type_impls;
     }
 })()
-//{"start":55,"fragment_lengths":[27]}
+//{"start":55,"fragment_lengths":[29]}
