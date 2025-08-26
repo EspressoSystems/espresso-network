2d4c7f5eb8e15cc6f9386444790e1c190dd1db37
ss-es <155648797+ss-es@users.noreply.github.com>
2025-08-26 13:58:35 -0400

    update

--- types/src/v0/impls/stake_table.rs
+++ types/src/v0/impls/stake_table.rs
@@ -2276,7 +2276,7 @@ impl Membership<SeqTypes> for EpochCommittees {
             block_height
         );
         let drb_leaf = peers
-            .try_fetch_leaf(1, block_height, stake_table, success_threshold)
+            .fetch_leaf(block_height, stake_table, success_threshold)
             .await?;
 
         let Some(drb) = drb_leaf.next_drb_result else {
