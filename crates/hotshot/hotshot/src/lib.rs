8763dc3cfeac3c592dc305c4a2616d5c537eaeaf
Brendon Fish <bfish713@gmail.com>
2025-08-28 15:46:58 -0400

    sort the epoch roots (#3552)

--- crates/hotshot/hotshot/src/lib.rs
+++ crates/hotshot/hotshot/src/lib.rs
@@ -1259,7 +1259,9 @@ async fn load_start_epoch_info<TYPES: NodeType>(
         .await
         .set_first_epoch(first_epoch_number, INITIAL_DRB_RESULT);
 
-    for epoch_info in start_epoch_info {
+    let mut sorted_epoch_info = start_epoch_info.clone();
+    sorted_epoch_info.sort_by_key(|info| info.epoch);
+    for epoch_info in sorted_epoch_info {
         if let Some(block_header) = &epoch_info.block_header {
             tracing::info!("Calling add_epoch_root for epoch {}", epoch_info.epoch);
 
