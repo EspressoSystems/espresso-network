a6395205ce16ca6a1ef18921d1e3ac527c7eed0d
sveitser <sveitser@gmail.com>
2025-09-17 17:01:25 +0200

    add IRewardClaim interface
    This is taken from PR
    
    https://github.com/EspressoSystems/espresso-network/pull/3491
    
    Would like to review and merge it into main first before we finish the
    contract so that it's easy to share with external parties.

---
+++ contracts/src/interfaces/IRewardClaim.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+interface IRewardClaim {
+    /// @notice Proof for rewards merkle tree. Obtained from Espresso query service API.
+    struct LifetimeRewardsProof {
+        bytes32[] siblings;
+    }
+
+    /// @notice User claimed rewards
+    event RewardClaimed(address indexed user, uint256 amount);
+
+    /// @notice Unable to authenticate rewards against Light Client contract
+    error InvalidAuthRoot();
+
+    /// @notice All available rewards already claimed
+    error AlreadyClaimed();
+
+    /// @notice Reward amount must be greater than zero
+    error InvalidRewardAmount();
+
+    /// @notice Claim staking rewards
+    ///
+    /// @param lifetimeRewards Total earned lifetime rewards for the user
+    /// @param proof Merkle proof attesting to lifetime rewards for the user
+    /// @param authRootInputs The authRootInputs must all be zero at the moment,
+    ///        this may change in the future with Espresso protocol upgrades.
+    ///
+    /// @notice Obtain rewards and proof from the Espresso query service API.
+    function claimRewards(
+        uint256 lifetimeRewards,
+        LifetimeRewardsProof calldata proof,
+        bytes32[7] calldata authRootInputs
+    ) external;
+
+    /// @notice Check amount of rewards claimed by a user
+    function claimedRewards(address claimer) external view returns (uint256);
+}
