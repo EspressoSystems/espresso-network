13696e363f418bde28a2048fd0a65755f8739b5f
ss-es <155648797+ss-es@users.noreply.github.com>
2025-09-18 11:03:00 -0400

    update

--- crates/hotshot/testing/tests/test_epoch_staggered_restart.rs
+++ crates/hotshot/testing/tests/test_epoch_staggered_restart.rs
@@ -67,7 +67,7 @@ cross_tests!(
           // Make sure we keep committing rounds after the catchup, but not the full 50.
           num_successful_views: 50,
           expected_view_failures: vec![8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 28],
-          possible_view_failures: vec![6, 7, 21, 22, 23, 24],
+          possible_view_failures: vec![6, 7, 21, 22, 23, 24, 25],
           decide_timeout: Duration::from_secs(120),
           ..Default::default()
       };
