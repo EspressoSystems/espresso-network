8f2770f6df300239dd4e3f8c645d6babd982b729
sveitser <sveitser@gmail.com>
2025-10-03 15:43:51 +0200

    fix: don't use deprecated method

--- staking-cli/src/main.rs
+++ staking-cli/src/main.rs
@@ -95,14 +95,8 @@ impl Args {
     }
 }
 
-fn is_json_logging() -> bool {
-    std::env::var("RUST_LOG_FORMAT")
-        .map(|v| v.eq_ignore_ascii_case("json"))
-        .unwrap_or(false)
-}
-
 fn output_success(msg: impl AsRef<str>) {
-    if is_json_logging() {
+    if std::env::var("RUST_LOG_FORMAT") == Ok("json".to_string()) {
         tracing::info!("{}", msg.as_ref());
     } else {
         println!("{}", msg.as_ref());
@@ -110,7 +104,7 @@ fn output_success(msg: impl AsRef<str>) {
 }
 
 fn output_error(msg: impl AsRef<str>) -> ! {
-    if is_json_logging() {
+    if std::env::var("RUST_LOG_FORMAT") == Ok("json".to_string()) {
         tracing::error!("{}", msg.as_ref());
     } else {
         eprintln!("{}", msg.as_ref());
