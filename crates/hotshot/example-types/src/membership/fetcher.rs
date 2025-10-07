47e922caa1ba87ddaadf4c5c9e1ebb6f9d2e8c86
ss-es <155648797+ss-es@users.noreply.github.com>
2025-10-07 12:51:50 -0400

    combine log messages

--- crates/hotshot/example-types/src/membership/fetcher.rs
+++ crates/hotshot/example-types/src/membership/fetcher.rs
@@ -129,9 +129,9 @@ impl<TYPES: NodeType> Leaf2Fetcher<TYPES> {
 
                         let Some(leaf) = leaves.get(&requested_height) else {
                             tracing::error!(
-                                "Block at height {requested_height} not found in storage"
+                                "Block at height {requested_height} not found in storage.\n\n \
+                                 stored leaves: {leaves:?}"
                             );
-                            tracing::error!("stored leaves: {leaves:?}");
                             continue;
                         };
 
