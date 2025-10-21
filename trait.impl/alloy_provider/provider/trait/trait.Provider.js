df0cabab802b07e7652702f14e509d0087431de3
alysiahuggins <alysiahuggins@users.noreply.github.com>
2025-10-21 14:46:41 +0000

    deploy: f1cecc9ff8672a491c28f2652bd37400a31980be

---
+++ trait.impl/alloy_provider/provider/trait/trait.Provider.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["espresso_types",[["impl Provider for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.L1Client.html\" title=\"struct espresso_types::v0::v0_1::L1Client\">L1Client</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[180]}
