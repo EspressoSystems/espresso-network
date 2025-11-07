826ea956b013ae4d42719750ee30d78e27251c86
Brendon Fish <bfish713@gmail.com>
2025-11-07 17:07:28 +0000

    retry w/o optimistic vid

--- crates/hotshot/task-impls/src/da.rs
+++ crates/hotshot/task-impls/src/da.rs
@@ -260,7 +260,7 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES>, V: Versions> DaTaskState<TYP
                 )
                 .await?;
 
-                tracing::error!("Sending vote to the DA leader {}", vote.view_number());
+                tracing::debug!("Sending vote to the DA leader {}", vote.view_number());
 
                 broadcast_event(Arc::new(HotShotEvent::DaVoteSend(vote)), &event_stream).await;
                 let mut consensus_writer = self.consensus.write().await;
@@ -288,76 +288,6 @@ impl<TYPES: NodeType, I: NodeImplementation<TYPES>, V: Versions> DaTaskState<TYP
                     tracing::trace!("{e:?}");
                 }
                 drop(consensus_writer);
-
-                // Optimistically calculate and update VID if we know that the primary network is down.
-                if self.network.is_primary_down() {
-                    let my_id = self.id;
-                    let consensus =
-                        OuterConsensus::new(Arc::clone(&self.consensus.inner_consensus));
-                    let pk = self.private_key.clone();
-                    let public_key = self.public_key.clone();
-                    let chan = event_stream.clone();
-                    let upgrade_lock = self.upgrade_lock.clone();
-                    let next_epoch = epoch_number.map(|epoch| epoch + 1);
-
-                    let mut target_epochs = vec![];
-                    if membership.has_stake(&public_key).await {
-                        target_epochs.push(epoch_number);
-                    }
-                    if membership
-                        .next_epoch_stake_table()
-                        .await?
-                        .has_stake(&public_key)
-                        .await
-                    {
-                        target_epochs.push(next_epoch);
-                    }
-                    if target_epochs.is_empty() {
-                        bail!(
-                            "Not calculating VID, the node doesn't belong to the current epoch or \
-                             the next epoch."
-                        );
-                    };
-
-                    tracing::debug!(
-                        "Primary network is down. Optimistically calculate own VID share."
-                    );
-                    let membership = membership.clone();
-                    spawn(async move {
-                        for target_epoch in target_epochs {
-                            Consensus::calculate_and_update_vid::<V>(
-                                OuterConsensus::new(Arc::clone(&consensus.inner_consensus)),
-                                view_number,
-                                target_epoch,
-                                membership.coordinator.clone(),
-                                &pk,
-                                &upgrade_lock,
-                            )
-                            .await;
-                            if let Some(vid_share) = consensus
-                                .read()
-                                .await
-                                .vid_shares()
-                                .get(&view_number)
-                                .and_then(|key_map| key_map.get(&public_key))
-                                .and_then(|epoch_map| epoch_map.get(&target_epoch))
-                            {
-                                tracing::debug!(
-                                    "Primary network is down. Calculated own VID share for epoch \
-                                     {target_epoch:?}, my id {my_id}"
-                                );
-                                broadcast_event(
-                                    Arc::new(HotShotEvent::VidShareRecv(
-                                        public_key.clone(),
-                                        vid_share.clone(),
-                                    )),
-                                    &chan,
-                                )
-                                .await;
-                            }
-                        }
-                    });
-                }
             },
             HotShotEvent::DaVoteRecv(ref vote) => {
                 tracing::debug!("DA vote recv, Main Task {}", vote.view_number());
