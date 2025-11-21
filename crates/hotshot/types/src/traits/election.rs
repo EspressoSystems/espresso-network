68580abc9d2e03f1de22e3e96a32fedb2f3d2270
Brendon Fish <bfish713@gmail.com>
2025-11-21 18:26:07 +0000

    use epoch for getting epoch root

--- crates/hotshot/types/src/traits/election.rs
+++ crates/hotshot/types/src/traits/election.rs
@@ -218,7 +218,7 @@ pub trait Membership<TYPES: NodeType>: Debug + Send + Sync {
     /// at the given block height
     fn get_epoch_root(
         _membership: Arc<RwLock<Self>>,
-        _block_height: u64,
+        _epoch: TYPES::Epoch,
     ) -> impl std::future::Future<Output = anyhow::Result<Leaf2<TYPES>>> + Send;
 
     /// Gets the DRB result for the given epoch
