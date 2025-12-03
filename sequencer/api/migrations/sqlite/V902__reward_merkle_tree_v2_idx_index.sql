9ed3b73a9fe017d068f62472cb33338dc2c935d9
imabdulbasit <baronbasit@gmail.com>
2025-12-04 00:42:36 +0500

    change query

--- sequencer/api/migrations/sqlite/V902__reward_merkle_tree_v2_idx_index.sql
+++ sequencer/api/migrations/sqlite/V902__reward_merkle_tree_v2_idx_index.sql
@@ -1,4 +1,4 @@
 -- Used by: get_all_reward_accounts() in sequencer/src/api/sql.rs
 
 CREATE INDEX reward_merkle_tree_v2_idx_created
-ON reward_merkle_tree_v2 (idx, created DESC);
+ON reward_merkle_tree_v2 (idx, created);
