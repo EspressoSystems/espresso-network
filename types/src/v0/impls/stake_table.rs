36bc75b8e45d6ca97433ab607c97d52b320ac720
Jeb Bearer <jeb@espressosys.com>
2025-12-08 12:28:20 -0800

    Make calculate_proportion_staked_and_reward_rate public

--- types/src/v0/impls/stake_table.rs
+++ types/src/v0/impls/stake_table.rs
@@ -1968,7 +1968,7 @@ impl EpochCommittees {
 ///         0.03 / sqrt(2 * p),            if 0.01 < p <= 1
 ///     }
 ///
-fn calculate_proportion_staked_and_reward_rate(
+pub fn calculate_proportion_staked_and_reward_rate(
     total_stake: &BigDecimal,
     total_supply: &BigDecimal,
 ) -> anyhow::Result<(BigDecimal, BigDecimal)> {
