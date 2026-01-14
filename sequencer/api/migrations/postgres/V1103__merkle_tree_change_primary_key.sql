fafe55065e3d5f795fe2c2d3026d1b3f6f214efb
ss-es <155648797+ss-es@users.noreply.github.com>
2026-01-14 11:11:34 -0500

    drop constraint if exists

--- sequencer/api/migrations/postgres/V1103__merkle_tree_change_primary_key.sql
+++ sequencer/api/migrations/postgres/V1103__merkle_tree_change_primary_key.sql
@@ -5,7 +5,7 @@
 ALTER TABLE 
   reward_merkle_tree
 DROP 
-  CONSTRAINT reward_merkle_tree_pkey;
+  CONSTRAINT IF EXISTS reward_merkle_tree_pkey;
 
 ALTER TABLE 
   reward_merkle_tree
@@ -17,7 +17,7 @@ ADD
 ALTER TABLE 
   reward_merkle_tree_v2
 DROP 
-  CONSTRAINT reward_merkle_tree_v2_pkey;
+  CONSTRAINT IF EXISTS reward_merkle_tree_v2_pkey;
 
 ALTER TABLE 
   reward_merkle_tree_v2
@@ -29,7 +29,7 @@ ADD
 ALTER TABLE 
   fee_merkle_tree
 DROP 
-  CONSTRAINT fee_merkle_tree_pkey;
+  CONSTRAINT IF EXISTS fee_merkle_tree_pkey;
 
 ALTER TABLE 
   fee_merkle_tree
@@ -41,7 +41,7 @@ ADD
 ALTER TABLE 
   block_merkle_tree
 DROP 
-  CONSTRAINT block_merkle_tree_pkey;
+  CONSTRAINT IF EXISTS block_merkle_tree_pkey;
 
 ALTER TABLE 
   block_merkle_tree
