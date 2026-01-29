d52464222bf02b199c814a807fb795f6ad7aba4d
ss-es <155648797+ss-es@users.noreply.github.com>
2026-01-29 05:11:54 -0500

    add migrations

--- sequencer/api/migrations/postgres/V1103__reward_merkle_tree_v2_bincode.sql
+++ sequencer/api/migrations/postgres/V1103__reward_merkle_tree_v2_bincode.sql
@@ -2,3 +2,14 @@ CREATE TABLE reward_merkle_tree_v2_bincode (
     height BIGINT PRIMARY KEY,
     serialized_bytes BYTEA
 );
+
+CREATE TABLE reward_merkle_tree_v2_proofs (
+    height BIGINT,
+    account BYTEA NOT NULL,
+    serialized_bytes BYTEA
+);
+
+ALTER TABLE 
+  reward_merkle_tree_v2_proofs 
+ADD 
+  CONSTRAINT reward_merkle_tree_v2_proofs_pk PRIMARY KEY (height, account);
--- sequencer/api/migrations/sqlite/V903__reward_merkle_tree_v2_bincode.sql
+++ sequencer/api/migrations/sqlite/V903__reward_merkle_tree_v2_bincode.sql
@@ -2,3 +2,10 @@ CREATE TABLE reward_merkle_tree_v2_bincode (
     height BIGINT PRIMARY KEY,
     serialized_bytes BLOB
 );
+
+CREATE TABLE reward_merkle_tree_v2_proofs (
+    height BIGINT,
+    account BLOB NOT NULL,
+    serialized_bytes BLOB,
+    PRIMARY KEY (height, account)
+);
