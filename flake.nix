be83e39693fb7af6897be689a23ca4f16f8652b3
ss-es <155648797+ss-es@users.noreply.github.com>
2026-02-06 09:42:38 -0500

    add separate shell for docker

--- flake.nix
+++ flake.nix
@@ -235,7 +235,6 @@
             prek
             prek-as-pre-commit # compat to allow running pre-commit
             entr
-            docker
             process-compose
             lazydocker # a docker compose TUI
             # `postgresql` defaults to an older version (15), so we select the latest version (16)
@@ -277,14 +276,23 @@
             # with rustup installations.
             export CARGO_HOME=$HOME/.cargo-nix
 
-            # Required for demo-native to run with docker-rootless
-            export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
-
             ${pre-commit.shellHook}
           '';
           RUST_SRC_PATH = "${stableToolchain}/lib/rustlib/src/rust/library";
           FOUNDRY_SOLC = "${solc}/bin/solc";
         });
+        devShells.dockerShell = pkgs.mkShell {
+          inputsFrom = [ self.devShells.${system}.default ];
+          packages = [ pkgs.docker ];
+          shellHook = lib.concatStringsSep "\n" [ 
+            self.devShells.${system}.default
+
+            ''
+            # Required for demo-native to run with docker-rootless
+            export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+            ''
+          ];
+        };
       devShells.crossShell =
         crossShell { config = "x86_64-unknown-linux-musl"; };
       devShells.armCrossShell =
