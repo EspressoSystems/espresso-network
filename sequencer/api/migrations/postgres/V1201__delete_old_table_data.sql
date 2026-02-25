bb5463cb53bc6b996fa699fc502b97e4c04b4739
imabdulbasit <baronbasit@gmail.com>
2026-02-25 16:30:30 +0500

    delete old tables data

---
+++ sequencer/api/migrations/postgres/V1201__delete_old_table_data.sql
@@ -0,0 +1,4 @@
+TRUNCATE da_proposal;
+TRUNCATE vid_share;
+TRUNCATE quorum_proposals;
+TRUNCATE quorum_certificate;
---
+++ sequencer/api/migrations/sqlite/V1001__delete_old_table_data.sql
@@ -0,0 +1,4 @@
+DELETE FROM da_proposal;
+DELETE FROM vid_share;
+DELETE FROM quorum_proposals;
+DELETE FROM quorum_certificate;
