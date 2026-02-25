5cde4c5c2f82956790b898a3343ee35740e734cd
imabdulbasit <baronbasit@gmail.com>
2026-02-25 22:46:20 +0500

    add comments

--- sequencer/api/migrations/sqlite/V1001__delete_old_table_data.sql
+++ sequencer/api/migrations/sqlite/V1001__delete_old_table_data.sql
@@ -1,3 +1,6 @@
+-- Delete data from old consensus tables
+-- The data has been migrated to the new tables (anchor_leaf2, da_proposal2,
+-- vid_share2, quorum_proposals2, quorum_certificate2)
 DELETE FROM anchor_leaf;
 DELETE FROM da_proposal;
 DELETE FROM vid_share;
