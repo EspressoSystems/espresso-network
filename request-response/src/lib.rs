749960c3b77350f9fae4ef48ff9ad3892f6ec23f
Brendon Fish <bfish713@gmail.com>
2026-03-05 18:28:27 +0000

    limit incomming responses by request hash, don't validate if the request is fulfilled

--- request-response/src/lib.rs
+++ request-response/src/lib.rs
@@ -29,6 +29,8 @@ use tokio_util::task::AbortOnDropHandle;
 use tracing::{debug, error, info, trace, warn};
 use util::{BoundedVecDeque, NamedSemaphore, NamedSemaphoreError};
 
+use crate::util::NamedSemaphorePermit;
+
 /// The data source trait. Is what we use to derive the response data for a request
 pub mod data_source;
 /// The message type. Is the base type for all messages in the request-response protocol
@@ -55,7 +57,7 @@ pub type OutgoingRequestsMap<Req> =
 pub type IncomingRequests<K> = NamedSemaphore<K>;
 
 /// A type alias for the list of tasks that are validating incoming responses
-pub type IncomingResponses = NamedSemaphore<()>;
+pub type IncomingResponses = NamedSemaphore<RequestHash>;
 
 /// The type of request to make
 #[derive(PartialEq, Eq, Clone, Copy)]
@@ -510,7 +512,9 @@ impl<
             self.config.max_incoming_requests_per_key,
             Some(self.config.max_incoming_requests),
         );
-        let mut incoming_responses = NamedSemaphore::new(self.config.max_incoming_responses, None);
+        // process 1 response per key, with a global maximum of [`config.max_incoming_responses`] responses
+        let mut incoming_responses =
+            NamedSemaphore::new(1, Some(self.config.max_incoming_responses));
 
         // While the receiver is open, we receive messages and handle them
         loop {
@@ -633,6 +637,32 @@ impl<
         });
     }
 
+    async fn wait_for_permit(
+        incoming_responses: &mut IncomingResponses,
+        request_hash: RequestHash,
+        outgoing_requests: &OutgoingRequestsMap<Req>,
+    ) -> Result<NamedSemaphorePermit<RequestHash>, NamedSemaphoreError> {
+        while outgoing_requests.read().contains_key(&request_hash) {
+            let permit = incoming_responses.try_acquire(request_hash);
+            let permit = match permit {
+                Ok(permit) => permit,
+                Err(NamedSemaphoreError::PerKeyLimitReached) => {
+                    tokio::time::sleep(Duration::from_millis(100)).await;
+                    continue;
+                },
+                Err(NamedSemaphoreError::GlobalLimitReached) => {
+                    warn!(
+                        "Failed to process response: too many responses are already being \
+                         processed"
+                    );
+                    return Err(NamedSemaphoreError::GlobalLimitReached);
+                },
+            };
+            return Ok(permit);
+        }
+        Err(NamedSemaphoreError::GlobalLimitReached)
+    }
+
     /// Handle a response sent to us
     fn handle_response(
         self: &Arc<Self>,
@@ -641,28 +671,37 @@ impl<
     ) {
         trace!("Handling response {response:?}");
 
-        // Get the entry in the map, ignoring it if it doesn't exist
-        let Some(outgoing_request) = self
-            .outgoing_requests
-            .read()
-            .get(&response.request_hash)
-            .cloned()
-            .and_then(|r| r.upgrade())
-        else {
-            return;
-        };
-
-        // Attempt to acquire a permit for the request. Warn if there are too many responses currently being processed
-        let permit = incoming_responses.try_acquire(());
-        let Ok(permit) = permit else {
-            warn!("Failed to process response: too many responses are already being processed");
-            return;
-        };
+        let outgoing_requests_clone = self.outgoing_requests.clone();
+        let mut incoming_responses_clone = incoming_responses.clone();
 
         // Spawn a task to validate the response and send it to the requester (us)
         let response_validate_timeout = self.config.incoming_response_timeout;
         tokio::spawn(async move {
             if timeout(response_validate_timeout, async move {
+                // Attempt to acquire a permit for the request. Warn if there are too many responses currently being processed
+                let permit = Self::wait_for_permit(
+                    &mut incoming_responses_clone,
+                    response.request_hash,
+                    &outgoing_requests_clone,
+                )
+                .await;
+
+                let Ok(permit) = permit else {
+                    warn!(
+                        "Failed to process response: too many responses are already being \
+                         processed"
+                    );
+                    return;
+                };
+                // Get the entry in the map, ignoring it if it doesn't exist
+                let Some(outgoing_request) = outgoing_requests_clone
+                    .read()
+                    .get(&response.request_hash)
+                    .cloned()
+                    .and_then(|r| r.upgrade())
+                else {
+                    return;
+                };
                 // Make sure the response is valid for the given request
                 let validation_result = match (outgoing_request.response_validation_fn)(
                     &outgoing_request.request,
