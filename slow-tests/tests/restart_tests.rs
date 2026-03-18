1ee0e18b9fe12daea1ffe5d9623b7ff1ce0b1d4f
Toralf Wittner <tw@dtex.org>
2026-03-18 17:07:01 +0100

    Fix slow restart tests.

--- slow-tests/tests/restart_tests.rs
+++ slow-tests/tests/restart_tests.rs
@@ -49,6 +49,7 @@ use hotshot_types::{
     network::{Libp2pConfig, NetworkConfig},
     signature_key::{BLSPrivKey, BLSPubKey},
     traits::signature_key::SignatureKey,
+    x25519,
 };
 use itertools::Itertools;
 use sequencer::{
@@ -69,6 +70,7 @@ use sequencer::{
 };
 use staking_cli::demo::{DelegationConfig, StakingTransactions};
 use surf_disco::{Url, error::ClientError};
+use tagged_base64::TaggedBase64;
 use tempfile::TempDir;
 use test_utils::reserve_tcp_port;
 use tokio::{
@@ -231,8 +233,10 @@ struct NetworkParams<'a> {
 struct NodeParams {
     api_port: u16,
     libp2p_port: u16,
+    cliquenet_port: u16,
     staking_key: PrivKey,
     state_key: StateKeyPair,
+    x25519_key: x25519::Keypair,
     is_da: bool,
 }
 
@@ -241,8 +245,10 @@ impl NodeParams {
         Ok(Self {
             api_port: reserve_tcp_port()?,
             libp2p_port: reserve_tcp_port()?,
+            cliquenet_port: reserve_tcp_port()?,
             staking_key: PubKey::generated_from_seed_indexed([0; 32], i).1,
             state_key: StateKeyPair::generate_from_seed_indexed([0; 32], i),
+            x25519_key: x25519::Keypair::generated_from_seed_indexed([0; 32], i)?,
             is_da,
         })
     }
@@ -303,6 +309,10 @@ impl<S: TestableSequencerDataSource> TestNode<S> {
                 .to_tagged_base64()
                 .expect("valid tagged-base64")
                 .to_string(),
+            "--private-x25519-key",
+            &TaggedBase64::try_from(node.x25519_key.secret_key())
+                .expect("valid key")
+                .to_string(),
             "--genesis-file",
             &network.genesis_file.display().to_string(),
             "--orchestrator-url",
@@ -311,6 +321,8 @@ impl<S: TestableSequencerDataSource> TestNode<S> {
             &format!("0.0.0.0:{}", node.libp2p_port),
             "--libp2p-advertise-address",
             &format!("127.0.0.1:{}", node.libp2p_port),
+            "--cliquenet-bind-address",
+            &format!("0.0.0.0:{}", node.cliquenet_port),
             "--cdn-endpoint",
             &format!("127.0.0.1:{}", network.cdn_port),
             "--state-peers",
