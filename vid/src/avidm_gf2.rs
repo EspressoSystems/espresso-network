e1443255e871ff95059b02e5cd5288f423cf344a
mrain <linmrain@gmail.com>
2026-04-24 12:21:53 -0400

    fix build

--- vid/src/avidm_gf2.rs
+++ vid/src/avidm_gf2.rs
@@ -343,8 +343,8 @@ impl VidScheme for AvidmGf2Scheme {
             }
 
             let result = decoder.decode()?;
-            for i in 0..original_count {
-                let shard: &[u8] = match input_orig[i] {
+			for (i, shard) in input_orig.iter().enumerate().take(original_count) {
+                let shard: &[u8] = match shard {
                     Some(data) => data,
                     None => result.restored_original(i).ok_or_else(|| {
                         VidError::Internal(anyhow!("Failed to recover the payload."))
