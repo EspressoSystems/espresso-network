d015da3d0b038002e38e6548c9b3f75a119fb724
ss-es <155648797+ss-es@users.noreply.github.com>
2026-05-07 13:13:09 -0400

    Add availability endpoint to axum API (#4216)

--- crates/espresso/api/build.rs
+++ crates/espresso/api/build.rs
@@ -13,12 +13,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .extern_path(".espresso.api.v2", "::serialization_api::v2")
         .file_descriptor_set_path(out_dir.join("reflection_descriptor.bin"))
         .compile_protos(
-            &["v2/common.proto", "v2/rewards.proto"],
+            &[
+                "v2/common.proto",
+                "v2/rewards.proto",
+                "v2/data.proto",
+                "v2/consensus.proto",
+            ],
             &[proto_root.to_str().unwrap()],
         )?;
 
     println!("cargo:rerun-if-changed=../../serialization/api/proto/v2/common.proto");
     println!("cargo:rerun-if-changed=../../serialization/api/proto/v2/rewards.proto");
+    println!("cargo:rerun-if-changed=../../serialization/api/proto/v2/data.proto");
+    println!("cargo:rerun-if-changed=../../serialization/api/proto/v2/consensus.proto");
 
     Ok(())
 }
