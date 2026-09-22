1a8bf886ee4fa0f2e0cf3de8085d343141ca00a1
Jeb Bearer <jeb@espressosys.com>
2026-09-21 19:31:04 -0700

    Increase cliquenet max size for real

--- crates/cliquenet/src/lib.rs
+++ crates/cliquenet/src/lib.rs
@@ -65,7 +65,7 @@ pub struct Config {
     peer_budget: NonZeroUsize,
 
     /// Max. number of bytes per message to send or receive.
-    #[builder(default = NonZeroUsize::new(10485760).expect("32000000 > 0"))]
+    #[builder(default = NonZeroUsize::new(32000000).expect("32000000 > 0"))]
     max_message_size: NonZeroUsize,
 
     /// Connect retry delays in seconds.
