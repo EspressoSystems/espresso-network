e9c736ac7a3166f51aa1ee941e4d9f5f7b2d9c3c
sveitser <sveitser@gmail.com>
2026-09-23 13:05:15 +0000

    chore(l1): default subscription timeout to 30s

--- crates/espresso/types/src/v0/v0_1/l1.rs
+++ crates/espresso/types/src/v0/v0_1/l1.rs
@@ -134,7 +134,7 @@ pub struct L1ClientOptions {
     #[clap(
         long,
         env = "ESPRESSO_L1_SUBSCRIPTION_TIMEOUT",
-        default_value = "1m",
+        default_value = "30s",
         value_parser = parse_duration,
     )]
     pub subscription_timeout: Duration,
